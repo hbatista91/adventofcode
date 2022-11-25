7e99d7ede8b08629dbf5392b14e4a930bc883551
Hugo Batista <hugomgbatista.91@gmail.com>
2022-11-25 09:20:15 +0000

    Added day 1 solution (2015 edition)

--- src/AdventOfCode2015/Day01.cs
+++ src/AdventOfCode2015/Day01.cs
@@ -3,7 +3,24 @@
     public class Day01
     {
         [Theory]
-        [InlineData("")]
+        [InlineData("(())",0)]
+        [InlineData("()()", 0)]
+        [InlineData("(((", 3)]
+        [InlineData("(()(()(", 3)]
+        [InlineData("))(((((", 3)]
+        [InlineData("())", -1)]
+        [InlineData("))(", -1)]
+        [InlineData(")))", -3)]
+        [InlineData(")())())", -3)]
+        public void ValidatePart1(string input, int expected)
+        {
+            var result = ExecuteLogicPart1(new string[] {input});
+
+            Assert.Equal(expected.ToString(), result.ToString());
+        }
+
+        [Theory]
+        [InlineData("138")]
         public void Part1(string expected)
         {
             var lines = Helpers.FetchContent(1, true);
@@ -14,22 +31,66 @@
 
         private object ExecuteLogicPart1(string[] lines)
         {
-            throw new NotImplementedException();
+            var floor = 0;
+            foreach (var item in lines[0].ToArray())
+            {
+                switch (item)
+                {
+                    case '(':
+                        floor++;
+                        break;
+                    case ')':
+                        floor--;
+                        break;
+                }
+            }
+
+            return floor.ToString();
         }
 
         [Theory]
-        [InlineData("")]
+        [InlineData(")", 1)]
+        [InlineData("()())", 5)]
+        public void ValidatePart2(string input, int expected)
+        {
+            var result = ExecuteLogicPart2(new string[] { input });
+
+            Assert.Equal(expected.ToString(), result.ToString());
+        }
+
+        [Theory]
+        [InlineData("1771")]
         public void Part2(string expected)
         {
             var lines = Helpers.FetchContent(1, false);
             var result = ExecuteLogicPart2(lines);
 
-            Assert.Equal(expected, result);
+            Assert.Equal(expected.ToString(), result.ToString());
         }
 
         private object ExecuteLogicPart2(string[] lines)
         {
-            throw new NotImplementedException();
+            var floor = 0;
+            var index = 1;
+            foreach (var item in lines[0].ToArray())
+            {
+                switch (item)
+                {
+                    case '(':
+                        floor++;
+                        break;
+                    case ')':
+                        floor--;
+                        break;
+                }
+                if (floor == -1)
+                {
+                    return index;
+                }
+                index++;
+            }
+
+            return floor.ToString();
         }
     }
 }
